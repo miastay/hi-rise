621b2624b3837caa5945c47898231ac60ae47823
Mia Taylor <miastaylor1@gmail.com>
2024-05-21 23:59:26 -0700

    build first static site

---
+++ docs/_app/immutable/entry/start.DME5PZan.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BD49C447.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"; 
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = "production" === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +9,15 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            //base: dev ? "" : "/hi-rise",
+            relative: true
+        },
 	},
     preprocess: [vitePreprocess()]
 };
